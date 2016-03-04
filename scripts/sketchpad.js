5dfc53c8f9427d42861e0b61a392fc01f68d001b
Andrew Rudnev <andrewrudnev@gmail.com>
2016-03-04 16:32:04 -0500

    16x16 grid of divs is made and they change color when mouse hovers over

---
+++ scripts/sketchpad.js
@@ -0,0 +1,21 @@
+var rows = 16;
+var columns = 16;
+var $row = $('<div></div>', {
+	class: 'row'
+});
+
+var $column = $('<div></div>', {
+	class: 'column'
+});
+
+$(document).ready(function() {
+
+	//columns move to the side 
+	for(var i = 0; i < columns; i++) {
+		$row.append($column.clone());
+	}
+	//rows move up and down 
+	for(var i = 0; i < rows; i++) {
+		$("#container").append($row.clone());
+	}
+});
